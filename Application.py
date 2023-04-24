c14909a1a04f0281059212b23a5c04737cdf2086
gavinpastreich <124082256+gavinpastreich@users.noreply.github.com>
2023-04-24 14:26:40 -0400

    Update Application.py

--- Application.py
+++ Application.py
@@ -2,7 +2,6 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
